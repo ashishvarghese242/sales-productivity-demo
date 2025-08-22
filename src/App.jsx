c66fe3231178ff09c98540d59b822c154a745b36
ashishvarghese242 <ashish.varghese242@gmail.com>
2025-08-22 15:14:47 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ const LEVERS = [
 ]
 
 // Visual tuning for enablement overlay
-const LRS_OVERLAY_MULTIPLIER = 1.9
+const LRS_OVERLAY_MULTIPLIER = 2
 
 // ------------------------------ Data hooks ------------------------------
 function useData() {
